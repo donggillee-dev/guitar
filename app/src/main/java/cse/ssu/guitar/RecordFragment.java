44e0345e83964f8343b87bebbbf50c6cc9d7d6b0
minwoo <zox2323@naver.com>
2018-08-03 18:08:51 +0900

    Update Record file

--- app/src/main/java/cse/ssu/guitar/RecordFragment.java
+++ app/src/main/java/cse/ssu/guitar/RecordFragment.java
@@ -1,13 +1,24 @@
 package cse.ssu.guitar;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import java.io.File;
 
 /**
  * Created by 성민우 on 2018-08-01.
@@ -16,10 +27,97 @@ public class RecordFragment extends Fragment {
     public static RecordFragment newInstance() {
         return new RecordFragment();
     }
+
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXETERNAL_STORAGE = 1;
     ToggleButton listenBtn;
+    Button play, stop;
+    // Environment.getExternalStorageDirectory()로 각기 다른 핸드폰의 내장메모리의 디렉토리를 알수있다.
+    final private static File RECORDED_FILE = Environment.getExternalStorageDirectory();
+
+    String filename;
+    // MediaPlayer 클래스에 재생에 관련된 메서드와 멤버변수가 저장어되있다.
+    MediaPlayer player;
+    // MediaRecorder 클래스에  녹음에 관련된 메서드와 멤버 변수가 저장되어있다.
+    MediaRecorder recorder;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.record_fragment, container, false);
+
+        filename = RECORDED_FILE.getAbsolutePath() + "/test.mp4";
+        ;
         listenBtn = (ToggleButton) view.findViewById(R.id.listenBtn);
+        play = (Button) view.findViewById(R.id.play);
+        stop = (Button) view.findViewById(R.id.stop);
+
+
+        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.RECORD_AUDIO);
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            // 권한 없음
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+        }
+        permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+            // 권한 없음
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_WRITE_EXETERNAL_STORAGE);
+        }
+        listenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (listenBtn.isChecked() == true) {
+                    if (recorder != null) {
+                        recorder.stop();
+                        recorder.release();
+                        recorder = null;
+                    }
+
+                    // 실험 결과 왠만하면 아래 recorder 객체의 속성을 지정하는 순서는 이대로 하는게 좋다 위치를 바꿨을때 에러가 났었음
+                    // 녹음 시작을 위해  MediaRecorder 객체  recorder를 생성한다.
+                    recorder = new MediaRecorder();
+                    // 오디오 입력 형식 설정
+                    recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                    recorder.setAudioSamplingRate(44100);
+                    recorder.setAudioEncodingBitRate(96000);
+                    // 음향을 저장할 방식을 설정
+                    recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+                    // 오디오 인코더 설정
+                    recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+                    // 저장될 파일 지정
+                    recorder.setOutputFile(filename);
+                    try {
+                        Toast.makeText(getActivity(), "녹음이 시작되었습니다.", Toast.LENGTH_LONG).show();
+
+                        // 녹음 준비,시작
+                        recorder.prepare();
+                        recorder.start();
+                    } catch (Exception ex) {
+                        Log.e("SampleAudioRecorder", "Exception : ", ex);
+                    }
+
+                } else {
+                    if (recorder == null)
+                        return;
+
+                    recorder.stop();
+                    recorder.release();
+                    recorder = null;
+
+                    Toast.makeText(getActivity(),
+                            "녹음이 중지되었습니다.", Toast.LENGTH_LONG).show();
+                    // TODO Auto-generated method stub
+
+
+                }
+
+            }
+        });
+
         return view; // 여기서 UI를 생성해서 View를 return
     }
 }
